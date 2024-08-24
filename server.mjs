f2285d30aad1533fdf209871ceed61d43465bc79
BarryWhite007 <34497183+BarryWhite007@users.noreply.github.com>
2024-08-24 11:59:07 +1000

    Add files via upload

--- server.mjs
+++ server.mjs
@@ -8,7 +8,131 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+//NEW
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
+const environment = process.env.ENVIRONMENT || 'sandbox';
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const endpoint_url = environment === 'sandbox' ? 'https://api-m.sandbox.paypal.com' : 'https://api-m.paypal.com';
+
+
+app.post('/create_order', (req, res) => {
+    get_access_token()
+        .then(access_token => {
+            let order_data_json = {
+                'intent': req.body.intent.toUpperCase(),
+                'purchase_units': [{
+                    'amount': {
+                        'currency_code': 'USD',
+                        'value': '100.00'
+                    }
+                }]
+            };
+            const data = JSON.stringify(order_data_json)
+
+            fetch(endpoint_url + '/v2/checkout/orders', { //https://developer.paypal.com/docs/api/orders/v2/#orders_create
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${access_token}`
+                    },
+                    body: data
+                })
+                .then(res => res.json())
+                .then(json => {
+                    res.send(json);
+                }) //Send minimal data to client
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send(err)
+        })
+});
+
+app.post('/complete_order', (req, res) => {
+    get_access_token()
+        .then(access_token => {
+            fetch(endpoint_url + '/v2/checkout/orders/' + req.body.order_id + '/' + req.body.intent, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${access_token}`
+                    }
+                })
+                .then(res => res.json())
+                .then(json => {
+                    console.log(json);
+                    let intent_object = req.body.intent === "authorize" ? "authorizations" : "captures";
+                    //Remove this if you don't want to send email with SendGrid
+                  if (json.purchase_units[0].payments[intent_object][0].status === "COMPLETED") {
+                      send_email_receipt({"id": json.id, "email": req.body.email});
+                    }
+                    res.send(json);
+                }) //Send minimal data to client
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send(err)
+        })
+});
+
+app.post("/get_client_token", (req, res) => {
+    get_access_token()
+      .then((access_token) => {
+        const payload = req.body.customer_id
+          ? JSON.stringify({ customer_id: req.body.customer_id })
+          : null;
+  
+        fetch(endpoint_url + "/v1/identity/generate-token", {
+          method: "post",
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+            "Content-Type": "application/json",
+          },
+          body: payload,
+        })
+          .then((response) => response.json())
+          .then((data) => res.send(data.client_token));
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        res.status(500).send("An error occurred while processing the request.");
+      });
+  });
+
+app.get("/.well-known/apple-developer-merchantid-domain-association", (req, res) => {
+  res.sendFile(process.cwd() + '/apple-developer-merchantid-domain-association');
+});
+
+//Servers the script.js file
+//app.get('/script.js', (req, res) => {
+    //res.sendFile(process.cwd() + '/script.js');
+//});
+
+
+
+function get_access_token() {
+    const auth = `${client_id}:${client_secret}`
+    const data = 'grant_type=client_credentials'
+    return fetch(endpoint_url + '/v1/oauth2/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': `Basic ${Buffer.from(auth).toString('base64')}`
+            },
+            body: data
+        })
+        .then(res => res.json())
+        .then(json => {
+            return json.access_token;
+        })
+}
+
+//NEW
 
 
 // render checkout page with client id & unique client token
