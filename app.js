f2b5e40808444068e0023d26ef68cfa53012688a
BarryWhite007 <34497183+BarryWhite007@users.noreply.github.com>
2024-08-24 11:37:19 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+PORT = 8888;
+server.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}/`);
+});
